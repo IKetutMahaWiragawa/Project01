c58bebe68e71dbd85b36e34ca5da554a4ae98ea6
IKetutMahaWiragawa <ketutmaha38@gmail.com>
2020-10-20 12:08:14 +0800

    Latihan Route menggunakan Controller'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\productController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aboutus', function(){
+    echo "Ini Merupakan Contoh Sebuah Page (Page About)";
+})->name("tentang");
+
+Route::get('/show/{id?}', function($id=1){
+    echo "Parameter ID: " .$id;
+})->where ('id','[0-9]+');
+
+Route::get('/utama', function(){
+    echo "Ini Page Utama";
+    echo "<br>";
+    echo "<a href='".route('tentang')."'>About</a>";
+});
+
+Route::get('/product',[productController::class,'index']);
