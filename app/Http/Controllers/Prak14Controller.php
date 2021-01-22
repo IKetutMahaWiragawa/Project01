a1957682d0333e33876dbcc061b546af907e451a
IKetutMahaWiragawa <ketutmaha38@gmail.com>
2021-01-22 22:53:59 +0800

    Percobaan

--- app/Http/Controllers/Prak14Controller.php
+++ app/Http/Controllers/Prak14Controller.php
@@ -16,24 +16,25 @@ class Prak14Controller extends Controller
      * 
      * @return \Illuminate\Http\Response
      */
-    
-     public function ChartProdperkat(){
-         $Kgrp = kategori::get();
-         $rl = array("role"=>"Blue");
-         $a = array("Element", "TotProd", $rl);
-         $dta = array($a);
-         
-         foreach ($Kgrp as $i => $v) {
-             $prd = produks::where('id_kat', $v->idkat)->get();
-             $jml = 0;
-             foreach ($prd as $i => $v) {
-                $jml += $vx->qty;
-                }
+
+    public function ChartProdperkat()
+    {
+        $Kgrp = kategori::get();
+        $rl = array("role" => "Blue");
+        $a = array("Element", "TotProd", $rl);
+        $dta = array($a);
+
+        foreach ($Kgrp as $i => $v) {
+            $prd = produks::where('id_kat', $v->idkat)->get();
+            $jml = 0;
+            foreach ($prd as $i => $v) {
+                $jml += $v->qty;
+            }
             array_push($dta, array($v->kategori, $jml, "yellow"));
-         }
-     return view('praktikum14.graph')
-        ->with('JNS', "BAR")
-        ->with('JUDULGRAFIK', "Total Produk per kategori")
-        ->with('DTA', json_encode($dta));
+        }
+        return view('praktikum14.graph')
+            ->with('JNS', "BAR")
+            ->with('JUDULGRAFIK', "Total Produk per kategori")
+            ->with('DTA', json_encode($dta));
     }
 }
--- resources/views/praktikum14/graph.blade.php
+++ resources/views/praktikum14/graph.blade.php
@@ -1,37 +1,48 @@
 @extends('prakgraph')
+@section('JUDULPAGE','Graph')
 @section('FOOTER')
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-    
-    google.charts.load("current", {packages:['corechart']});
+<script type="text/javascript">
+    google.charts.load("current", {
+        packages: ['corechart']
+    });
     google.charts.setOnLoadCallback(drawChart);
-    function drawchart(){
-        var data = google.visualization.arrayToDataTable({!!$DTA!!});
+
+    function drawchart() {
+        var data = google.visualization.arrayToDataTable({
+            !!$DTA!!
+        });
 
         var view = new google.visualization.DataView(data);
-        @if($JNS=="BAR")
-        view.setColumns([0,1,
-                        { clac: "stringlfy",
-                          sourceColumn: 1,
-                          type: "string",
-                          role: "annotation" },
-                        2]);
-        var options ={
+        @if($JNS == "BAR")
+        view.setColumns([0, 1,
+            {
+                clac: "stringlfy",
+                sourceColumn: 1,
+                type: "string",
+                role: "annotation"
+            },
+            2
+        ]);
+        var options = {
             title: "{{$JUDULGRAFIK}}",
             width: 600,
             height: 400,
-            bar: {groupWidth: "95%"},
-            legend: { position: "none" },
+            bar: {
+                groupWidth: "95%"
+            },
+            legend: {
+                position: "none"
+            },
         };
         var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
         @else
         var options = {
             title: "{{$JUDULGRAFIK}}"
-         };
-         var chart = new google.visualization.Pie(document.getElementById("columnchart_values"));
+        };
+        var chart = new google.visualization.Pie(document.getElementById("columnchart_values"));
         @endif
         chart.draw(view, options);
     }
-
-    </script>
-    @endsection
+</script>
+@endsection
